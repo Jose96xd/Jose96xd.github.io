b326dbcd5b68e4aac965eeb5e0a050cd89adc113
Jose96 <jose96xd@gmail.com>
2020-10-25 10:37:50 +0100

    Added an animation to the "toTop" button and change it to make it invisible when the user is at the top of the page.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
+// This function moves the user view to the top of the web smoothly
 function toTop()
 {
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
+// This function makes the button disappear when the user is in the top of the web.
+window.addEventListener('scroll', function(){
+  var scroll = document.querySelector('.fab');
+  scroll.classList.toggle("active", window.scrollY > 100);
+})
