35f7cd47592a0550b9d6afadb034aeafb6279b8d
Jose96 <jose96xd@gmail.com>
2020-10-31 12:05:25 +0100

    Added the wordTimeApi to get the current day of the year.

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,24 @@ function toTop()
 {
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
+
 // This function makes the button disappear when the user is in the top of the web.
 window.addEventListener('scroll', function(){
   var scroll = document.querySelector('.fab');
   scroll.classList.toggle("active", window.scrollY > 100);
 })
+
+
+// This method calls the displayCurrentTime function when loading the web.
+window.onload = function() {
+  displayCurrentTime();
+}
+
+// Method that ask the wordltimeapi what day of the year is and displays it under the generic html picture.
+function displayCurrentTime(){
+  //var data = "http://worldtimeapi.org/api/timezone/Europe/Madrid";
+  $.getJSON("http://worldtimeapi.org/api/timezone/Europe/Madrid", function(data) {
+      document.getElementById("apiDataHolder").innerHTML="Day: " + data.day_of_year + " of 365.";
+}); 
+    
+}   
